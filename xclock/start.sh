b53cb24e17f21745402df70ffc07a3b507beff54
Karl Schultz <karl@lunarg.com>
2017-04-06 16:45:10 -0600

    Saving my initial docker files.

---
+++ xclock/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+	--name xclock \
+	-e DISPLAY=$DISPLAY \
+	-e NVIDIA_DRIVER_VERSION=340 \
+	--device /dev/nvidiactl \
+	--device /dev/nvidia0 \
+	--device /dev/snd \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $XAUTHORITY:/tmp/.host_Xauthority:ro \
+	-dti karlschultzlunarg/xclock
